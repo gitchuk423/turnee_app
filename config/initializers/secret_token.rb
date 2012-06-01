ad5f7d2abd6394657ac83cad950223a00d9f5aac
Junkchuk <junkchuk@gmail.com>
2012-05-31 20:57:49 -0400

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurneeApp::Application.config.secret_token = 'bff83d20ada1ffbcdde707f2894b8ff766ba26857c939fd93d87de6d4ac39390a2cbb8b5a6bb7e5b2a12f1871cca6d578a3e72bb0ca493d3c69857d4885898e9'
