bfde50a981a034d3dd7956f9945f74a4f49163f6
Joe Holovachuk <junkchuk@gmail.com>
2012-06-05 23:45:09 -0400

    db/migrate/20120602004647_create_attorneys.rb

--- db/migrate/20120602004647_create_attorneys.rb
+++ db/migrate/20120602004647_create_attorneys.rb
@@ -1,8 +1,15 @@
 class CreateAttorneys < ActiveRecord::Migration
   def change
     create_table :attorneys do |t|
-
       t.timestamps
+    end
+    
+    create_table :personal_record do |t|
+    	t.string :attorney_id 
+    	t.string :email
+    	t.string :first_name
+    	t.string :last_name
+    	t.timestamps
     end
   end
 end
