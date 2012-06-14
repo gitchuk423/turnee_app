66356fd4b686987e75760ecafc486e795b0adf2d
Joe Holovachuk <junkchuk@gmail.com>
2012-06-14 00:02:36 -0400

    Changed default for current in professional experience migration to false from 0 for compliance with posgres

--- db/migrate/20120614023904_add_position_to_professional_experience.rb
+++ db/migrate/20120614023904_add_position_to_professional_experience.rb
@@ -1,6 +1,6 @@
 class AddPositionToProfessionalExperience < ActiveRecord::Migration
   def change
     add_column :professional_experiences, :position, :string
-    add_column :professional_experiences, :current, :boolean, default: 0
+    add_column :professional_experiences, :current, :boolean, default: false
   end
 end
